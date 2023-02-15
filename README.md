1e57d574300414aec980a4c5c880769ff16662f7
Guillaume Pierson <guillaumep@marmelab.com>
2023-02-15 17:34:39 +0100

    Fix build

---
+++ README.md
@@ -0,0 +1,1 @@
+The project does use Tensorflow and model has been created from https://teachablemachine.withgoogle.com/train/audio
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // plugins: [...]
   base: "/voiracing/",
-};
+  resolve: {
+    alias: {
+      util: "rollup-plugin-node-polyfills/polyfills/util",
+    },
+  },
+});
